c9230827e8dbb3187a0448d5c271caa45e9bc28f
janibehm <jani.behm@gmail.com>
2024-11-15 18:14:09 +0200

    merge main

--- .vercel/output/static/_astro/hoisted.CqvbUzBM.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.CWJ47YpE.js";const s=new IntersectionObserver(e=>{e.forEach(r=>{r.isIntersecting&&r.target.classList.add("show")})});document.querySelectorAll(".fade-in").forEach(e=>s.observe(e));
