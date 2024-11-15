f8452681ae32fdf8fec3cbeb49cbe978d0219b4d
janibehm <jani.behm@gmail.com>
2024-11-16 00:21:40 +0200

    add padding left to scroll content

--- .vercel/output/static/_astro/ContactForm.0SZCM5SU.js
+++
@@ -1,1 +0,0 @@
-import{default as p}from"./ContactForm.CSWCMKMX.js";import"./jsx-runtime.C0Vhfwdg.js";import"./index.DhYZZe0J.js";export{p as default};
--- .vercel/output/static/_astro/hoisted.BWrQZHgT.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.BvZ3JVE_.js";const s=new IntersectionObserver(e=>{e.forEach(r=>{r.isIntersecting&&r.target.classList.add("show")})});document.querySelectorAll(".fade-in").forEach(e=>s.observe(e));
