b99b5715856ae6fb9b1eca6d41a80c8396fb2673
janibehm <jani.behm@gmail.com>
2024-11-21 22:20:32 +0200

    image optimizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
